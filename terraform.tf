bf25d7d2125bbbc1f9ff23a0890169c482e56691
Alper Çıtak <citak.alper@gmail.com>
2023-02-24 21:42:18 +0300

    updated nginx image

--- terraform.tf
+++ terraform.tf
@@ -10,22 +10,22 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "nginx" {
-  name         = "nginx:latest"
+  name         = "nginx:1.23.3-alpine-slim"
   keep_locally = false
 }
 
 resource "docker_container" "nginx" {
-  image = "${docker_image.nginx.name}"
+  image = docker_image.nginx.name
   name  = "node-docker-terraform-nginx"
 
   must_run          = true
   restart           = "always"
   publish_all_ports = false
-  depends_on        = [
+  depends_on = [
     docker_container.app1,
     docker_container.app2
   ]
-  links             = [
+  links = [
     "node-docker-terraform-app-1:node-docker-terraform-app-1",
     "node-docker-terraform-app-2:node-docker-terraform-app-2"
   ]
@@ -37,63 +37,61 @@ resource "docker_container" "nginx" {
   }
 
   upload {
-    content = "${file("nginx.conf")}"
+    content = file("nginx.conf")
     file    = "/etc/nginx/nginx.conf"
   }
 }
 
-resource "docker_image" "app_image" {
-  name = "app_image"
+resource "docker_image" "node-docker-terraform-image" {
+  name = "node-docker-terraform-image"
 
-  build {  
-    path        = "."
-    dockerfile  = "Dockerfile"
-    target      = "deploy"
+  build {
+    path       = "."
+    dockerfile = "Dockerfile"
+    target     = "deploy"
   }
 }
 
 resource "docker_container" "app1" {
-  image = "${docker_image.app_image.image_id}"
+  image = docker_image.node-docker-terraform-image.image_id
   name  = "node-docker-terraform-app-1"
 
-  env               = ["NODE_PORT=3001", "APP_NAME=app1"]
-  user              = "node"
-  entrypoint        = ["nohup", "node", "./index.js", "&"]
+  env = ["NODE_PORT=3001", "APP_NAME=app1"]
+  # entrypoint        = ["nohup", "node", "./index.js", "&"]
   must_run          = true
   restart           = "always"
   publish_all_ports = true
 
 
   upload {
-    content = "${file("package.json")}"
+    content = file("package.json")
     file    = "app/package.json"
   }
 
   upload {
-    content = "${file("index.js")}"
+    content = file("index.js")
     file    = "app/index.js"
   }
 }
 
 resource "docker_container" "app2" {
-  image = "${docker_image.app_image.image_id}"
+  image = docker_image.node-docker-terraform-image.image_id
   name  = "node-docker-terraform-app-2"
 
-  env               = ["NODE_PORT=3002", "APP_NAME=app2"]
-  user              = "node"
-  entrypoint        = ["nohup", "node", "./index.js", "&"]
+  env = ["NODE_PORT=3002", "APP_NAME=app2"]
+  # entrypoint        = ["nohup", "node", "./index.js", "&"]
   must_run          = true
   restart           = "always"
   publish_all_ports = true
 
 
   upload {
-    content = "${file("package.json")}"
+    content = file("package.json")
     file    = "app/package.json"
   }
 
   upload {
-    content = "${file("index.js")}"
+    content = file("index.js")
     file    = "app/index.js"
   }
 }
